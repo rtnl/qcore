189a6c6a0e4ebf19d02bc7c58ecc3baf923e3338
Rashaad <rsblunt@gmail.com>
2017-05-01 16:45:07 -0500

    helloworld

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Q | rational.godlist</title>
+    <link href="style/godlist.css" rel="stylesheet" type="text/css">
+	</head>
+
+	<body>
+		<div id="top" >RATIONAL</div>
+	</body>
+</html>
